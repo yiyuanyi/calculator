bd90b843f7be3c44bd82e4e66ad1490821f49347
Matt H <59845559+yiyuanyi@users.noreply.github.com>
2020-09-22 22:37:58 +0100

    initial commit. basic operation functions written and tested, working correctly.

---
+++ script.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a,b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a/b;
+};
+
+function operate(operator, num1, num2) {
+    let a = num1;
+    let b = num2;
+    switch(operator) {
+        case '+':
+            return add(a,b);
+            break;
+        case '-':
+            return subtract(a,b);
+            break;
+        case '*':
+            return multiply(a,b);
+            break;
+        case '/':
+            return divide(a,b);
+            break;
+    };
+}
