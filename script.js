2e6d96cafa248fd0b6bf759f8167deae9c0435a4
Matt H <59845559+yiyuanyi@users.noreply.github.com>
2020-09-30 20:07:57 +0100

    display now selects html results screen. added result variable.

--- script.js
+++ script.js
@@ -17,44 +17,51 @@ function divide(a, b) {
 let a;
 let b;
 let operator;
+let result;
+
+const display = document.getElementById('result');
+
+document.getElementById('buttons').addEventListener('click', function(e) {
+    if (e.target && e.target.matches('button.btn.num')) {
+        if (operator == undefined) {
+        a = parseInt(e.target.innerHTML);
+        display.innerHTML += a;
+        } else {
+            b = parseInt(e.target.innerHTML);
+            display.innerHTML += b;
+        }
+    } else if (e.target && e.target.matches('button.btn.operator')) {
+        operator = (e.target.id).toString();
+        console.log(operator);
+        display.innerHTML += e.target.innerHTML;
+    }
+});
 
 function operate(operator, a, b) {
     switch(operator) {
-        case add:
+        case 'add':
             return add(a,b);
             break;
-        case subtract:
+        case 'subtract':
             return subtract(a,b);
             break;
-        case multiply:
+        case 'multiply':
             return multiply(a,b);
             break;
-        case divide:
+        case 'divide':
             return divide(a,b);
             break;
     };
 };
 
-const result = document.getElementById('result');
-
-document.getElementById('buttons').addEventListener('click', function(e) {
-    if (e.target && e.target.matches('button.btn.num')) {
-        a = parseInt(e.target.innerHTML);
-        result.innerHTML += a;
-    } else if (e.target && e.target.matches('button.btn.operator')) {
-        operator = e.target.id;
-        console.log(e.target.id);
-    }
-});
-
 document.getElementById('clear').addEventListener('click', function(){
     a = '';
     b = '';
     operator = '';
-    result.innerHTML = '';
+    display.innerHTML = '';
 });
 
-document.getElementById('equals').addEventListener('click', operate(operator, a, b));
+document.getElementById('equals').addEventListener('click', operate);
 
 
 
